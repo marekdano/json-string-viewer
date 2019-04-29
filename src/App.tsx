1b9ae6b1015fe5a99a607e40aa853aa4a0f4386a
Marek Dano <mk.dano@gmail.com>
2019-04-29 14:19:07 +0100

    Replace textarea html elements within JSONEditorArea components in the App component.

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import './App.css';
+import JSONEditorArea from './components/JSONEditorArea';
 
 const App: React.FC = () => {
+  const defaultJSON = {
+    'array': [1, 2, 3],
+    'boolean': true,
+    'null': null,
+    'number': 'four',
+    'object': {'a': 'b', 'c': 'd'},
+    'string': 'Hello World'
+  };
+
   return (
     <main>
-      <h2>JSON string validator and viewer <span>- paste JSON string to the left panel to see the result in the right one</span></h2>
+      <h2>JSON string Viewer<span className="subtitle"> - paste JSON string to the left panel to see the result in the right one</span></h2>
       <section>
-        <textarea id="jsonInput" placeholder="Enter JSON string"></textarea>
+        <JSONEditorArea json={defaultJSON} />
         <section className="action-buttons">
           <button id="generalJSON">Validate & Prettify JSON string</button>
         </section>
-        <textarea id="convertedJSON"></textarea>
+        <JSONEditorArea json={defaultJSON} />
       </section>
     </main>
   );
