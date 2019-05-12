c40986cdc0fde64e196429720d078a512de6235b
Marek Dano <mk.dano@gmail.com>
2019-05-12 14:40:33 +0100

    Add type to JSONEditorArea components and change defaultJSON in App component.

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import JSONEditorArea from './components/JSONEditorArea';
 import parseJsonString from './utils/parseJsonString';
 
-const defaultJSON = "{\"array\": [1, 2, 3],\"boolean\": true,\"null\": null, \"number\": \"four\",\"string\": \"Hello World\"}";
+const defaultJSON = '';
 
 const App: React.FC = () => {
   const [jsonString, setJsonString] = useState(defaultJSON);
@@ -26,11 +26,11 @@ const App: React.FC = () => {
     <main>
       <h2>JSON string Viewer<span className="subtitle"> - paste JSON string to the left panel to see the result in the right one</span></h2>
       <section>
-        <JSONEditorArea json={jsonString} onChangeJson={onChangeJsonString} />
+        <JSONEditorArea json={jsonString} type='input' onChangeJson={onChangeJsonString} />
         <section className="action-buttons">
           <button id="generalJSON" onClick={onJsonValidation}>Validate & Prettify JSON string</button>
         </section>
-        <JSONEditorArea json={!jsonOutput ? '' : jsonOutput} onChangeJson={() => {}}/>
+        <JSONEditorArea json={!jsonOutput ? '' : jsonOutput} type='output' onChangeJson={() => {}} />
       </section>
     </main>
   );
