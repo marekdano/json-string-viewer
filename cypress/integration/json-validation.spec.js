00f737b44137916f05d355a6b1c7d05ace026503
Marek Dano <mk.dano@gmail.com>
2019-05-29 16:54:10 +0100

    Add npm scripts to execute tests. Add cypress test for checking valid JSON object.

--- cypress/integration/json-validation.spec.js
+++ cypress/integration/json-validation.spec.js
@@ -28,7 +28,31 @@ describe('validate & prettify', () => {
 
 	it('should display error message when invalid json string entered.', () => {
 		input = '\"{{}\\"array\\": [1, 2, 3],\\"boolean\\\\": true,\\"null\\": null, \\"number\\": \\"four\\",\\"string\\": \\"Hello World again\\"}"';
-		output = 'Unexpected end of JSON input';
+		output = "Parse error on line 1:\n..., 2, 3],\\\"boolean\\\\\": true,\\\"null\\\": nul\n-----------------------^\nExpecting 'EOF', '}', ',', ']', got ':'";
+
+		cy.visit('/')
+			.get('#input > .jsoneditor > .jsoneditor-outer > .ace_editor > textarea')
+			.clear({force: true})
+			.type(input, {force: true})
+			.getByText(/Validate & Prettify JSON string/i)
+			.click()
+			.get('#output > .jsoneditor > .jsoneditor-menu > .jsoneditor-compact')
+			.click()
+			.get('#output > .jsoneditor > .jsoneditor-outer > .ace_editor > .ace_scroller > .ace_content > .ace_layer.ace_text-layer > .ace_line_group > .ace_line')
+			.should(($div) => {
+				const values = $div.map((i, el) => Cypress.$(el).text())
+				const result = values
+					.get()
+					.map(line => line.trimStart())
+					.join('')
+			
+				expect(result).to.eq(JSON.stringify(output))
+			});
+	}); 
+
+	it('should display error message when valid JSON is entered.', () => {
+		input = `{{}"array": [1, 2, 3], "string": "Hello World again"}`;
+		output =   "Unexpected token o in JSON at position 1";
 
 		cy.visit('/')
 			.get('#input > .jsoneditor > .jsoneditor-outer > .ace_editor > textarea')
