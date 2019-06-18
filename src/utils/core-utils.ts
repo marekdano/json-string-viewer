8b512914a348866bb079b862593f678725112668
Marek Dano <mk.dano@gmail.com>
2019-06-18 17:08:09 +0100

    Add util method of downloadFile to the core-utils.

---
+++ src/utils/core-utils.ts
@@ -0,0 +1,24 @@
+import { JSONObject } from "../App";
+
+export const downloadFile = (fileContent: JSONObject, filename: string = 'default_name') => {
+  const blob = new Blob([JSON.stringify(fileContent, null, 2)], {
+    type: 'application/json'
+  });
+
+  if (window.navigator.msSaveOrOpenBlob) {
+    navigator.msSaveOrOpenBlob(blob, `${filename}.json`);
+  } else {
+    const link = document.createElement('a');
+    link.style.display = 'none';
+    document.body.appendChild(link);
+
+    if (link.download !== undefined) {
+      link.setAttribute('href', URL.createObjectURL(blob));
+      link.setAttribute('download', `${filename}.json`);
+      link.click();
+    } else {
+      window.open(encodeURI('data:application/json'));
+    }
+    document.body.removeChild(link);
+  }
+};
